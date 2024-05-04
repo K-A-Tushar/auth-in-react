8619c6b9f4a485f5e4d16221c5b6bc685bfac6b7
KAWSAR AHMED Tushar <149403668+K-A-Tushar@users.noreply.github.com>
2024-05-04 22:51:54 +0600

    database update

--- API/database.php
+++ API/database.php
@@ -4,9 +4,18 @@ header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
-define("API", "http://localhost/ReactJS/PROJECT/furni/API/");
-$conn = new mysqli("localhost", "root", "", "web_ecomm");
-//error handling
+define("API", "http://localhost/ReactJS/PROJECT/auth-in-react/API/");
+
+// database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "web_ecomm";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
